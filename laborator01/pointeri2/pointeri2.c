454a18df867f899d629c6d2e8f75dc525428b3af
laviniaraduta <lavinia.maria.raduta07@gmail.com>
2023-03-05 14:03:58 +0200

    Seconf example for pointers

---
+++ laborator01/pointeri2/pointeri2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+#define ARR_LENGTH 5
+ 
+int main()
+{
+   int *arr, i;
+ 
+   arr = (int *)malloc(sizeof(int) * ARR_LENGTH);
+   // arr = (int *)calloc(ARR_LENGTH, sizeof(int));
+ 
+   for (i = 0; i < ARR_LENGTH; ++i) {
+       /*
+        * arr + i iterează prin adresele fiecărui element din tablou însă adresa arr + i nu crește cu i ci cu i * sizeof(int), arr fiind un pointer la int
+        * în C nu se vede și nu este necesară această operație dar mai târziu în limbajul de asamblare va fi necesar 
+        */
+       printf("arr[%d] = %d -  la adresa %p\n", i, *(arr + i), arr + i);
+   }
+ 
+   free(arr);
+   return 0;
+}
